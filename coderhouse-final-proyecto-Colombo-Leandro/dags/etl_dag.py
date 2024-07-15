4942b7b0e6ce70f7c6cd383adf3744eb07e09f70
LeandroColombo213 <leandro@remitz.com>
2024-07-15 19:05:10 -0300

    readme new

--- coderhouse-final-proyecto-Colombo-Leandro/dags/etl_dag.py
+++ coderhouse-final-proyecto-Colombo-Leandro/dags/etl_dag.py
@@ -1,12 +1,17 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
+from modules.extraction import extract_posts
+from modules.loading import load_initial_data, load_combined_data
+from modules.transformation import transform_and_combine
+from modules.alerting import send_email
+import pandas as pd
+from dotenv import load_dotenv
 
-from modules.extraction import extract_api_data, extract_db_data
-from modules.transformation import transform_data
-from modules.loading import load_data_to_dwh
-from modules.alerting import send_alert
+# Cargar las variables de entorno
+load_dotenv()
 
+# Definir los argumentos por defecto del DAG
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -17,12 +22,68 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-dag = DAG('etl_pipeline', default_args=default_args, schedule_interval=timedelta(days=1))
+# Definir el DAG
+dag = DAG(
+    'etl_dag',
+    default_args=default_args,
+    description='ETL DAG for loading data to Redshift',
+    schedule_interval=timedelta(days=1),
+)
 
-extract_api = PythonOperator(task_id='extract_api', python_callable=extract_api_data, dag=dag)
-extract_db = PythonOperator(task_id='extract_db', python_callable=extract_db_data, dag=dag)
-transform = PythonOperator(task_id='transform', python_callable=lambda: transform_data(extract_api.output, extract_db.output), dag=dag)
-load = PythonOperator(task_id='load', python_callable=load_data_to_dwh, dag=dag)
-alert = PythonOperator(task_id='alert', python_callable=send_alert, dag=dag)
+def extract():
+    posts = extract_posts()
+    return posts
 
-extract_api >> extract_db >> transform >> load >> alert
+def load_initial(posts, **kwargs):
+    excel_file_path = 'dataframe/dataframe.xls'
+    users_df = pd.read_excel(excel_file_path)
+    load_initial_data(posts, users_df)
+
+def transform(**kwargs):
+    combined_data = transform_and_combine()
+    return combined_data
+
+def load_combined(**kwargs):
+    combined_data = kwargs['ti'].xcom_pull(task_ids='transform')
+    load_combined_data(combined_data)
+
+def send_alert(**kwargs):
+    send_email("ETL Process Completed", "The ETL process has completed successfully.")
+
+# Definir las tareas del DAG
+t1 = PythonOperator(
+    task_id='extract',
+    python_callable=extract,
+    dag=dag,
+)
+
+t2 = PythonOperator(
+    task_id='load_initial',
+    python_callable=load_initial,
+    provide_context=True,
+    dag=dag,
+)
+
+t3 = PythonOperator(
+    task_id='transform',
+    python_callable=transform,
+    provide_context=True,
+    dag=dag,
+)
+
+t4 = PythonOperator(
+    task_id='load_combined',
+    python_callable=load_combined,
+    provide_context=True,
+    dag=dag,
+)
+
+t5 = PythonOperator(
+    task_id='send_alert',
+    python_callable=send_alert,
+    provide_context=True,
+    dag=dag,
+)
+
+# Definir el flujo de tareas
+t1 >> t2 >> t3 >> t4 >> t5
--- readme.md
+++ readme.md
@@ -1,14 +1,43 @@
+# Proyecto de ETL con Airflow y Redshift
 
-**ESTRUCTURA DE MI PROYECTO
+Este proyecto implementa un flujo de trabajo ETL (Extract, Transform, Load) utilizando Apache Airflow para orquestar las tareas y Amazon Redshift como la base de datos de destino.
 
-proyecto/
-├── dags/
-│   └── etl_dag.py
-│
-├── modules/
-│   ├── extraction.py
-│   ├── transformation.py
-│   ├── loading.py
-│   ├── alerting.py
-│
-└── requirements.txt
+## Estructura del Proyecto
+
+
+### Descripción de los Directorios y Archivos
+
+- **dags/**: Contiene los archivos del DAG de Airflow.
+  - `etl_dag.py`: Define el DAG para el flujo de trabajo ETL.
+- **modules/**: Contiene los módulos de Python que realizan las operaciones de extracción, transformación, carga y alertas.
+  - `extraction.py`: Define la función `extract_posts()` para extraer datos desde una API.
+  - `transformation.py`: Define la función `transform_and_combine()` para transformar y combinar datos.
+  - `loading.py`: Define las funciones `load_initial_data()` y `load_combined_data()` para cargar datos en Amazon Redshift.
+  - `alerting.py`: Define la función `send_email()` para enviar alertas por correo electrónico.
+- **requirements.txt**: Lista de dependencias necesarias para el proyecto.
+
+## Requisitos
+
+Asegúrate de tener instaladas las siguientes dependencias. Puedes instalarlas utilizando `pip`:
+
+```bash
+pip install -r requirements.txt
+
+
+
+Configuración
+Variables de Entorno
+Crea un archivo .env en la raíz del proyecto con las siguientes variables de entorno:
+
+REDSHIFT_USER=
+REDSHIFT_PASSWORD=
+REDSHIFT_ENDPOINT=
+REDSHIFT_PORT=
+REDSHIFT_DB=
+REDSHIFT_TABLE=
+ALERT_EMAIL=your_email@gmail.com
+RECEIVER_EMAIL=receiver_email@gmail.com
+EMAIL_PASSWORD=your_email_password
+
+
+Este `README.md` proporciona una guía completa sobre la configuración y ejecución del proyecto ETL con Airflow y Redshift, incluyendo la estructura del proyecto, los requisitos, y el código del DAG de Airflow(dentro de la carpeta /dags).
